be0554c374285da50aaf0c8864e75e258b95081e
willog11 <willog11@gmail.com>
2018-01-20 15:33:20 +0000

    Tweaking params

--- src/cost.cpp
+++ src/cost.cpp
@@ -6,7 +6,7 @@
 #include <math.h>
 
 const float REACH_GOAL = pow(10, 5);
-const float EFFICIENCY = pow(10, 6);
+const float EFFICIENCY = pow(10, 4);
 const float COMFORT = pow(10, 5);
 
 float lane_change_cost(const Vehicle & vehicle, const vector<Vehicle> & trajectory, const map<int, vector<Vehicle>> & predictions, map<string, float> & data) {
