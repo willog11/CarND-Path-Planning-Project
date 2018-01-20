200ac80d2c69f103d6bf1f5ff644d1d1bd2f9ec8
willog11 <willog11@gmail.com>
2018-01-20 11:29:30 +0000

    Fixing bug in previous commit

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -132,8 +132,7 @@ vector<float> Vehicle::get_kinematics(map<int, vector<Vehicle>> predictions, int
 
 			float max_velocity_in_front = (vehicle_ahead.s - this->s - this->preferred_buffer) + vehicle_ahead.v * 0.02 - 0.5 * (this->a); // Equation: d = d0+ vt -0.5a(t^2)
 			//float max_velocity_in_front = (vehicle_ahead.s + vehicle_ahead.v * 0.02 - this->s); // Equation: v = s1-s0/t
-            new_velocity = min(min(max_velocity_in_front, max_velocity_accel_limit), this->target_speed);
-			float new_accel =  (new_velocity - this->v) / 0.02;
+            new_velocity = min(min(max_velocity_in_front, max_velocity_accel_limit), this->target_speed); 
 			//std::cout << "[VEH] NVB: Vehical ahead s: " << vehicle_ahead.s << endl;
 			//std::cout << "[VEH] NVB: Vehical ahead velocity: " << vehicle_ahead.v << endl;
 			//std::cout << "[VEH] NVB: New velocity: " << new_velocity << endl;
@@ -142,6 +141,7 @@ vector<float> Vehicle::get_kinematics(map<int, vector<Vehicle>> predictions, int
 			//std::cout << "[VEH] NVB:Max velocity in front: " << max_velocity_in_front << endl;
         }
 
+		new_accel = (new_velocity - this->v) / 0.02;
 		if (abs(new_accel) > this->max_acceleration)
 		{
 			new_velocity = this->v - (this->max_acceleration * 0.02);
