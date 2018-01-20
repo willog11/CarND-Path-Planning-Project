da4b410d033a65067297df1afff59a45a7bf8cec
willog11 <willog11@gmail.com>
2018-01-20 15:42:24 +0000

    Tweaking cost weights

--- src/cost.cpp
+++ src/cost.cpp
@@ -7,7 +7,7 @@
 
 const float REACH_GOAL = pow(10, 5);
 const float EFFICIENCY = pow(10, 5);
-const float COMFORT = pow(10, 3);
+const float COMFORT = pow(10, 2);
 
 float lane_change_cost(const Vehicle & vehicle, const vector<Vehicle> & trajectory, const map<int, vector<Vehicle>> & predictions, map<string, float> & data) {
 	/*
