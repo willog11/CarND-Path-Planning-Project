e5e46079c43ffc674215128551c4b1ad21f658b1
willog11 <willog11@gmail.com>
2018-01-20 11:08:55 +0000

    Fine tuning deacceleration

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -129,20 +129,20 @@ vector<float> Vehicle::get_kinematics(map<int, vector<Vehicle>> predictions, int
 			std::cout << "[VEH] VB: Vehicle behind use velocity: " << new_velocity << endl;
         } else {
             //float max_velocity_in_front = (vehicle_ahead.s - this->s - this->preferred_buffer) + vehicle_ahead.v * 0.02 - 0.5 * (this->a) * 0.02 * 0.02; // Equation: d = d0+ vt -0.5a(t^2)
-			float max_velocity_in_front = (vehicle_ahead.s + vehicle_ahead.v * 0.02 - this->s - this->preferred_buffer) / 0.02; // Equation: v = s1-s0/t
+			float max_velocity_in_front = (vehicle_ahead.s + vehicle_ahead.v * 0.02 - this->s) / 0.02; // Equation: v = s1-s0/t
             new_velocity = min(min(max_velocity_in_front, max_velocity_accel_limit), this->target_speed);
-			float new_accel =  (this->v - new_velocity) / 0.02;
+			float new_accel =  (new_velocity - this->v) / 0.02;
 			std::cout << "[VEH] NVB: Vehical ahead s: " << vehicle_ahead.s << endl;
 			//std::cout << "[VEH] NVB: Vehical ahead velocity: " << vehicle_ahead.v << endl;
 			std::cout << "[VEH] NVB: New velocity: " << new_velocity << endl;
 			std::cout << "[VEH] NVB: New accel: " << new_accel << endl;
 			//std::cout << "[VEH] NVB: Max velocity with accel: " << max_velocity_accel_limit << endl;
 			std::cout << "[VEH] NVB:Max velocity in front: " << max_velocity_in_front << endl;
-			/*if (new_accel > this->max_acceleration)
+			if (abs(new_accel) > this->max_acceleration)
 			{
 				new_velocity = this->v - (this->max_acceleration * 0.02);
 				std::cout << "[VEH] NVB: Smoothing velocity" << endl;
-			}*/
+			}
         }
     } else {
         new_velocity = min(max_velocity_accel_limit, this->target_speed);
