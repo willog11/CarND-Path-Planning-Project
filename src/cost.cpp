623e8c90c0597d875a0a86f282020e9102e3e4a0
willog11 <willog11@gmail.com>
2018-01-20 12:09:22 +0000

    Readding goal cost fn

--- src/cost.cpp
+++ src/cost.cpp
@@ -70,8 +70,8 @@ float calculate_cost(const Vehicle & vehicle, const map<int, vector<Vehicle>> & 
     float cost = 0.0;
 
     //Add additional cost functions here.
-    vector< function<float(const Vehicle & , const vector<Vehicle> &, const map<int, vector<Vehicle>> &, map<string, float> &)>> cf_list = {inefficiency_cost};
-    vector<float> weight_list = {EFFICIENCY};
+    vector< function<float(const Vehicle & , const vector<Vehicle> &, const map<int, vector<Vehicle>> &, map<string, float> &)>> cf_list = {inefficiency_cost, goal_distance_cost };
+    vector<float> weight_list = {EFFICIENCY, REACH_GOAL };
     
     for (int i = 0; i < cf_list.size(); i++) {
         float new_cost = weight_list[i]*cf_list[i](vehicle, trajectory, predictions, trajectory_data);
