6b974f1380c4df86f7ce00e113d121b04edbad0c
willog11 <willog11@gmail.com>
2018-01-20 11:21:35 +0000

    Tweaking buffer sizes

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -250,7 +250,7 @@ bool Vehicle::get_vehicle_behind(map<int, vector<Vehicle>> predictions, int lane
     Returns a true if a vehicle is found behind the current vehicle, false otherwise. The passed reference
     rVehicle is updated if a vehicle is found.
     */
-    int max_gap = this->preferred_buffer;
+    int max_gap = 10;
     bool found_vehicle = false;
     Vehicle temp_vehicle;
     for (map<int, vector<Vehicle>>::iterator it = predictions.begin(); it != predictions.end(); ++it) {
@@ -271,7 +271,7 @@ bool Vehicle::get_vehicle_ahead(map<int, vector<Vehicle>> predictions, int lane,
     Returns a true if a vehicle is found ahead of the current vehicle, false otherwise. The passed reference
     rVehicle is updated if a vehicle is found.
     */
-    int min_s = this->preferred_buffer * 2 + this->s;
+    int min_s = this->preferred_buffer + this->s;
     bool found_vehicle = false;
     Vehicle temp_vehicle;
     for (map<int, vector<Vehicle>>::iterator it = predictions.begin(); it != predictions.end(); ++it) {
